7e73ae75091da8be62b6e81904f9788a414fcdbc
Geral <otacilio.system@gmail.com>
2022-03-07 16:38:12 -0400

    Initial commit

---
+++ core/views.py
@@ -0,0 +1,18 @@
+from django.views.generic import FormView, DetailView
+
+from .models import Funcionario, Servico
+
+
+class IndexView(FormView):
+    template_name = 'index.html'
+    # sobreescrevendo o método de obtenção do contexto
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['funcionarios'] = Funcionario.objects.order_by('?').all()
+        context['servicos'] = Servico.objects.order_by('?').all()
+        return context
+
+
+class DetalharServicoView(DetailView):
+    template_name = 'detalhe_servico.html'
+    model = Servico
