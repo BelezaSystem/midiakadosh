5f38bb0843ee5c4a2d447ca6c19e535e8558acfc
Geral <otacilio.system@gmail.com>
2022-03-07 18:00:41 -0400

    Atualização de Models e Views

--- core/views.py
+++ core/views.py
@@ -1,11 +1,14 @@
-from django.urls import reverse_lazy
 from django.views.generic import FormView, DetailView
+from django.urls import reverse_lazy
+from django.contrib import messages
 
 from .models import Funcionario, Servico
+from .forms import ContatoForm
 
 
 class IndexView(FormView):
     template_name = 'index.html'
+    form_class = ContatoForm
     success_url = reverse_lazy('core:index')
 
     # sobreescrevendo o método de obtenção do contexto
@@ -14,6 +17,15 @@ class IndexView(FormView):
         context['funcionarios'] = Funcionario.objects.order_by('?').all()
         context['servicos'] = Servico.objects.order_by('?').all()
         return context
+
+    def form_valid(self, form, *args, **kwargs):
+        form.send_mail()
+        messages.success(self.request,'E-mail enviado com sucesso!!!')
+        return super(IndexView, self).form_valid(form, *args, *kwargs)
+
+    def form_invalid(self, form, *args, **kwargs):
+        messages.error(self.request, 'Erro ao enviar o E-mail.')
+        return super(IndexView, self).form_invalid(form, *args, *kwargs)
 
 
 class DetalharServicoView(DetailView):
