211c0a84fd6dec314c9a0b697cb98434d87e08c1
Geral <otacilio.system@gmail.com>
2022-03-07 17:05:21 -0400

    Fatiando o Template

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'core'
 urlpatterns=[
     path('', views.IndexView.as_view(), name='index'),
-    #path('<int:pk>/', views.DetalharServicoView.as_view(), name='detalhe_servico')
+    path('<int:pk>/', views.DetalharServicoView.as_view(), name='detalhe_servico')
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+from django.urls import reverse_lazy
 from django.views.generic import FormView, DetailView
 
 from .models import Funcionario, Servico
@@ -5,6 +6,8 @@ from .models import Funcionario, Servico
 
 class IndexView(FormView):
     template_name = 'index.html'
+    success_url = reverse_lazy('core:index')
+
     # sobreescrevendo o método de obtenção do contexto
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
